d4d4c5010a2a00b2b477d22e925aeba31a1bd63b
Andrew Hamerly <73661602+andrewhamerly@users.noreply.github.com>
2024-07-02 16:08:23 -0400

    Created User and Thought Models and package.json dependencies.

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction.js');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+        text: String,
+        required: true,
+        maxLength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reaction.length;
+  });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
